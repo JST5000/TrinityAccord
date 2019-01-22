a20aee3a9d14516cf71c3f71e11dcf72e90fc1af
JST5000 <aim4math@gmail.com>
2019-01-22 10:34:05 -0500

    Added basic buttons for Hand, Deck, Discard, and End Turn. None are functional currently.

---
+++ Assets/CardHitbox.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardHitbox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
