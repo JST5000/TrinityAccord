076065292079025ff4ea532f6e22ffdbfc6402e7
JST5000 <aim4math@gmail.com>
2019-02-27 13:56:05 -0500

    Started working on the choose3 ui

---
+++ Assets/Scripts/Managers/Choose3Manager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Choose3Manager : MonoBehaviour
+{
+    private static CardManager selectedCard = null;
+    private CardManager[] options;
+
+    //TODO
+    public static void SetAndHighlightSelectedCard(CardManager newSelection)
+    {
+        if (selectedCard != null)
+        {
+            selectedCard.GetComponent<CardUIUpdater>().ResetHighlight();
+        }
+        selectedCard = newSelection;
+        if (newSelection != null)
+        {
+            selectedCard.GetComponent<CardUIUpdater>().Highlight();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        options = GetComponentsInChildren<CardManager>();
+        foreach(CardManager man in options)
+        {
+            man.Init(new Sword());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
