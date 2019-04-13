97b8dc4819dcce1b126d611ff31c178a25933fa5
JST5000 <aim4math@gmail.com>
2019-04-13 14:43:10 -0400

    Added a start menu, win screen scene, and started working on the help page.

---
+++ Assets/Scripts/Managers/MainMenuManger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManger : MonoBehaviour
+{
+
+    public GameObject Help;
+
+    public void StartNewGame()
+    { 
+        //Removes previous game data
+        GameObject permanentState = GameObject.Find("PermanentState");
+        if(permanentState != null)
+        {
+            Destroy(permanentState);
+        }
+        SceneManager.LoadScene("Encounter");
+    }
+
+    public void OpenHelp()
+    {
+        Instantiate(Help, GameObject.Find("Canvas").transform, false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
