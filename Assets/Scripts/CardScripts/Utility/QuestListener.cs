c4496d0633412036002baa8d0e5c48b8f8474d22
jst5000 <aim4math@gmail.com>
2019-11-26 22:45:22 -0500

    Added outlines to shops

---
+++ Assets/Scripts/CardScripts/Utility/QuestListener.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class holds functions that will slowly check for Quest conditions, then will update the encounter deck when they trigger.
+/// </summary>
+public class QuestListener : MonoBehaviour
+{
+    //This sets up the initial condition for all quests in for a new encounter
+    public void InitializeQuests()
+    {
+
+    }
+
+    //Apprentice
+    
+}
