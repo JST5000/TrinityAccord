d2770fbcf8514d0c462652707d128b0936b45da9
jst5000 <aim4math@gmail.com>
2019-12-27 18:43:06 -0500

    Added shorthand icons for when you aren't hovering enemies. Also made the 'about to attack' color more threatening!

---
+++ Assets/Scripts/Enemies/KeywordsToIcons.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class KeywordsToIcons : MonoBehaviour
+{
+    public TextMeshProUGUI effectText;
+    public TextMeshProUGUI iconShorthandText;
+
+    Dictionary<string, string> keywordsToIcons = InitKeywordsToIcons();
+
+    static Dictionary<string, string> InitKeywordsToIcons()
+    {
+        Dictionary<string, string> keyToIcon = new Dictionary<string, string>();
+        keyToIcon.Add("flip", InLineIcon.FLIP);
+        keyToIcon.Add(InLineIcon.ON_DISARM, InLineIcon.ON_DISARM);
+        keyToIcon.Add("summon", InLineIcon.SUMMON);
+        keyToIcon.Add("blind", InLineIcon.BLIND);
+        return keyToIcon;
+    }
+    
+    private string ExtractKeyIcons(string text)
+    {
+        string output = "";
+        char[] delimiters = { ' ', '\n', ':', ',' };
+        string[] tokens = text.Split(delimiters, System.StringSplitOptions.RemoveEmptyEntries);
+        foreach(string token in tokens)
+        {
+            keywordsToIcons.TryGetValue(token.ToLower(), out string translated);
+            if (translated != null && !output.Contains(translated))
+            {
+                output += translated;
+            }
+        }
+
+        return output;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        iconShorthandText.text = ExtractKeyIcons(effectText.text);
+    }
+}
--- Assets/Scripts/UIObjects/InLineIcon.cs
+++ Assets/Scripts/UIObjects/InLineIcon.cs
@@ -6,4 +6,7 @@ public static class InLineIcon
 {
     public static string DAMAGE = "<sprite=0>";
     public static string ON_DISARM = "<sprite=1>";
+    public static string SUMMON = "<sprite=2>";
+    public static string FLIP = "<sprite=3>";
+    public static string BLIND = "<sprite=4>";
 }
